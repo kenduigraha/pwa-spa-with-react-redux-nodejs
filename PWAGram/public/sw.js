f3e3250cb05182cc1e261ab989c6a79685079772
kenduigraha <ken.duigraha@kudo.co.id>
2017-11-13 10:15:18 +0700

    change event active to activate

--- PWAGram/public/sw.js
+++ PWAGram/public/sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service Worker', event);
 });
 
-self.addEventListener('active', function(event) {
+self.addEventListener('activate', function(event) {
     console.log('[Service Worker] Activating Service Worker', event);
     return self.clients.claim();
 });
