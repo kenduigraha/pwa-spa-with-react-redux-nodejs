c5d2c88f43c88140c59b9057633c86cb047098a6
kenduigraha <ken.duigraha@kudo.co.id>
2017-11-14 16:22:08 +0700

    ajax example with fetch and XMLhttpRequest

--- PWAGram/public/src/js/app.js
+++ PWAGram/public/src/js/app.js
@@ -60,3 +60,54 @@ promise
     });
 
 console.log('This is executed right after setTimeout');
+
+fetch('https://httpbin.org/ip')
+    .then(function(response) {
+        console.log(response)
+        return response.json();
+    })
+    .then(function(response) {
+        console.log(response);
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+
+fetch('https://httpbin.org/post', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        mode: "cors",
+        body: JSON.stringify(
+            {
+                message: "This is message to post"
+            }
+        )
+    })
+    .then(function(response) {
+        console.log(response)
+        return response.json();
+    })
+    .then(function(response) {
+        console.log(response);
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://httpbin.org/ip');
+xhr.responseType = 'json';
+
+xhr.onload = function() {
+    console.log('use xhr');
+    console.log(xhr.response);
+};
+
+xhr.onerror = function() {
+    console.log('error');
+};
+
+xhr.send();
