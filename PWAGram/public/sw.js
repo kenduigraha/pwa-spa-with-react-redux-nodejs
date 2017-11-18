1ea289261abf5c12d4b706be5494890a3783ea4c
kenduigraha <ken.duigraha@kudo.co.id>
2017-11-18 17:09:05 +0700

    addall caching asset files

--- PWAGram/public/sw.js
+++ PWAGram/public/sw.js
@@ -5,9 +5,23 @@ self.addEventListener('install', function(event) {
         caches.open('static')
               .then(function(cache) {
                   console.log('[Service Worker] Precaching App Shell');
-                  cache.add('/');
-                  cache.add('/index.html');
-                  cache.add('/src/js/app.js');
+                //   cache.add('/');
+                //   cache.add('/index.html');
+                //   cache.add('/src/js/app.js');
+                cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/src/js/app.js',
+                    '/src/js/feed.js',
+                    '/src/js/promise.js',
+                    '/src/js/fetch.js',
+                    '/src/css/app.css',
+                    '/src/css/feed.css',
+                    '/src/images/main-image.jpg',
+                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                    'https://fonts.googleapis.com/icon?family=Material+Icons',
+                    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+                ]);
               })
     );
 });
